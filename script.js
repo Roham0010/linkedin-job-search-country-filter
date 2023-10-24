9a3c82c5d24845bff2815370e7bf991f8593b7aa
Roham0010 <hasanshojaycyber@gmail.com>
2023-10-24 15:12:02 +0330

    improving and adding new features

--- script.js
+++ script.js
@@ -1,27 +1,86 @@
-const countriesToIgnore = [
-    'China', 'Lithuania', 'Czechia', 'Serbia', 'Romania', 'Brazil', 'Pakistan', 'India', 'poland', 
-    'applied', 'peru', 'bangladesh', 'russia', 'Philippines', 'drupal', 'latvia', 'nepal', 'kazakhstan', 
-    'hong kong', 'Managua', 'Georgia', 'Cambodia', 'hungary', 'slovakia', 'greater', 'mexico', 'africa'
-];
-const wordsNotToBeInTitle = ['angular', 'front-end', 'frontend', 'lead php'];
+// Linkedin
+const countriesToIgnore = ['China', 'Czechia', 'Serbia', 'Romania', 'Brazil'];
+// clearInterval(myInterval1);
+const wordsNotToBeInTitle = ['entwickler', 'magento', 'ruby', '.net', 'desarrollador', 'ontwikkelaar'];
+const companiesToIgnore = ['jobdesk', 'dexter'];
+
+// Get the viewed jobs and delete those added 5 days ago.
+var storageItems = JSON.parse(localStorage.getItem('jobTitleAndLocations'));
+if (storageItems) {
+    for(let date in storageItems) {
+        if(((new Date().getTime() - date) / (1000*60*60*24*5)) > 4) {
+            // delete storageItems[date];
+            console.log('deleted deleted deleted deleted deleted deleted deleted deleted deleted deleted ');
+        }
+    }
+} else {
+    storageItems = {};
+}
+
+// unchecks the following company on the review step of the easy apply
 setInterval(() => {
+    if(document.querySelectorAll('#follow-company-checkbox')[0] !== undefined) {
+        document.querySelectorAll('#follow-company-checkbox')[0].checked = false;
+    }
+
+}, 500);
+let firstTitleAndLocation = null;
+const myInterval1 = setInterval(() => {
     const allLocationsMeta = document.getElementsByClassName('job-card-container');
     const jobItemsListContainer = document.getElementsByClassName('scaffold-layout__list-container');
-    const titleAndLocations = [];
-
+    const titleAndCompanies = [];
+    
     for(i = 0; i < allLocationsMeta.length ; i++) {
         if (jobItemsListContainer[0].children.length < 2) continue;
         var locationMeta = allLocationsMeta[i];
-        const titleText = locationMeta.querySelector('.job-card-list__title').textContent.toLowerCase();
-        const locationText = locationMeta.querySelector('.job-card-container__primary-description').textContent.toLowerCase();
-        var titleAndLocation =  titleText + locationText;
-            
-        
-        if (wordsNotToBeInTitle.some(word => titleText.includes(word)) || 
-            titleAndLocations.includes(titleAndLocation) || 
-            countriesToIgnore.some(country => locationMeta.textContent.toLowerCase().includes(country.toLowerCase()))) {
-            locationMeta.closest('li.jobs-search-results__list-item').remove();
+        if (locationMeta.closest('li.jobs-search-results__list-item').style.display == 'none') continue;
+        if (locationMeta.closest('li.jobs-search-results__list-item').getAttribute('isChecked')) continue;
+        const titleText = locationMeta.querySelector('.job-card-list__title').textContent.toLowerCase().replace(/\s+/g,' ');
+        const companyText = locationMeta.querySelector('.job-card-container__primary-description').textContent.toLowerCase().replace(/\s+/g,' ');
+        const countryText = locationMeta.querySelector('.artdeco-entity-lockup__caption').textContent.toLowerCase().replace(/\s+/g,' ');
+        var titleAndCompany =  titleText + companyText;
+        if (i === 0 && firstTitleAndLocation !== titleAndCompany) {
+            firstTitleAndLocation = titleAndCompany;
+            console.log('Next page:______________________________________');
         }
-        titleAndLocations.push(titleAndLocation);
+        
+        let canRemove = false;
+        let reason = '';
+        let log = '';
+        if (wordsNotToBeInTitle.some(word => titleText.includes(word))) {
+            canRemove = true;
+            reason = 'wordsNotToBeInTitle';
+            log = reason + " => " + titleAndCompany;
+        } else if (companiesToIgnore.some(word => companyText.includes(word.toLowerCase()))) {
+            canRemove = true;
+            reason = 'CompanyIgnored';
+            log = reason + " => " + companyText;
+        } else if((Object.values(storageItems).indexOf(titleAndCompany) > -1)) {
+            canRemove = true;
+            reason = 'isInStorage';
+            log = reason + " => " + titleAndCompany;
+        } else if(titleAndCompanies.includes(titleAndCompany)) {
+            canRemove = true;
+            reason = 'Repetitieve';
+            log = reason + " => " + titleAndCompany;
+        } else if(countriesToIgnore.some(country => locationMeta.textContent.toLowerCase().includes(country.toLowerCase()))) {
+            canRemove = true;
+            reason = 'Country';
+            log = reason + " => " + countryText;
+        }
+        
+        if(canRemove) {
+            locationMeta.closest('li.jobs-search-results__list-item').style.display = 'none';
+            console.log(log);
+        } else {
+            if (!storageItems || !(Object.values(storageItems).indexOf(titleAndCompany) > -1)) {
+                let timestamp = new Date().getTime();
+                storageItems[timestamp] = titleAndCompany;
+                console.log('added: ', titleAndCompany);
+                localStorage.setItem('jobTitleAndLocations', JSON.stringify(storageItems));
+            }
+        }
+        titleAndCompanies.push(titleAndCompany);
+        locationMeta.closest('li.jobs-search-results__list-item').setAttribute('isChecked', true);
     }
 }, 1000)
